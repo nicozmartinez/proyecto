675bc09d25a01076bfe8b45aabbfaca5e46d938b
iancano <iancarp20022017@gmail.com>
2024-05-15 08:15:51 -0300

    init-project

---
+++ JS/productos.js
@@ -0,0 +1,67 @@
+const productos = [
+  {
+    id: "ANF-01",
+    title: "Anafe eléctrico Clever",
+    image: "../IMG/productos/anafeClaver.webp",
+    categorie: {
+      name: "Anafe",
+      type: "Electrica",
+      id: "cocina",
+    },
+    price: 35000,
+  },
+  {
+    id: "ANF-02",
+    title: "Anafe eléctrico Clever",
+    image: "../IMG/productos/anafeClaver.webp",
+    categorie: {
+      name: "Anafe",
+      type: "Electrica",
+      id: "cocina",
+    },
+    price: 35000,
+  },
+  {
+    id: "ANF-03",
+    title: "Anafe eléctrico Clever",
+    image: "../IMG/productos/anafeClaver.webp",
+    categorie: {
+      name: "Anafe",
+      type: "Electrica",
+      id: "cocina",
+    },
+    price: 35000,
+  },
+  {
+    id: "ANF-04",
+    title: "Anafe eléctrico Clever",
+    image: "../IMG/productos/anafeClaver.webp",
+    categorie: {
+      name: "Anafe",
+      type: "Electrica",
+      id: "cocina",
+    },
+    price: 35000,
+  },
+  {
+    id: "ANF-05",
+    title: "Anafe eléctrico Clever",
+    image: "../IMG/productos/anafeClaver.webp",
+    categorie: {
+      name: "Anafe",
+      type: "Electrica",
+      id: "cocina",
+    },
+  },
+  {
+    id: "ANF-06",
+    title: "Anafe eléctrico Clever",
+    image: "../IMG/productos/anafeClaver.webp",
+    categorie: {
+      name: "Anafe",
+      type: "Electrica",
+      id: "cocina",
+    },
+    price: 35000,
+  },
+];
